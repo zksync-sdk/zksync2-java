adecb5a14f6cd37658d027a702740d5cab2acbb5
MaximFischuk <mfischuk@vareger.com>
2021-10-20 13:24:34 +0300

    Add new Web3 RPC methods to ZkSync provider

--- src/main/java/io/zksync/protocol/JsonRpc2_0ZkSync.java
+++ src/main/java/io/zksync/protocol/JsonRpc2_0ZkSync.java
@@ -8,10 +8,16 @@ import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.JsonRpc2_0Web3j;
 import org.web3j.protocol.core.Request;
 import org.web3j.protocol.core.methods.response.EthGetBalance;
+import org.web3j.protocol.core.methods.response.EthSendRawTransaction;
 
 import io.zksync.methods.request.ZksEstimateFeeRequest;
+import io.zksync.methods.response.ZksAccountType;
 import io.zksync.methods.response.ZksEstimateFee;
+import io.zksync.methods.response.ZksIsTokenLiquid;
 import io.zksync.methods.response.ZksMainContract;
+import io.zksync.methods.response.ZksTokenPrice;
+import io.zksync.methods.response.ZksTokens;
+import io.zksync.methods.response.ZksTransactions;
 
 public class JsonRpc2_0ZkSync extends JsonRpc2_0Web3j implements ZkSync {
 
@@ -38,6 +44,42 @@ public class JsonRpc2_0ZkSync extends JsonRpc2_0Web3j implements ZkSync {
             Arrays.asList(address, defaultBlockParameter.getValue(), tokenAddress),
             web3jService,
             EthGetBalance.class);
+    }
+
+    @Override
+    public Request<?, EthSendRawTransaction> zksGetL1WithdrawalTx(String transactionHash) {
+        return new Request<>(
+                "zks_getL1WithdrawalTx", Arrays.asList(transactionHash), web3jService, EthSendRawTransaction.class);
+    }
+
+    @Override
+    public Request<?, ZksAccountType> zksGetAccountType(String address) {
+        return new Request<>(
+                "zks_getAccountType", Arrays.asList(address), web3jService, ZksAccountType.class);
+    }
+
+    @Override
+    public Request<?, ZksTransactions> zksGetAccountTransactions(String address, Integer before, Short limit) {
+        return new Request<>(
+                "zks_getAccountTransactions", Arrays.asList(address, before, limit), web3jService, ZksTransactions.class);
+    }
+
+    @Override
+    public Request<?, ZksTokens> zksGetConfirmedTokens(Integer from, Short limit) {
+        return new Request<>(
+                "zks_getConfirmedTokens", Arrays.asList(from, limit), web3jService, ZksTokens.class);
+    }
+
+    @Override
+    public Request<?, ZksIsTokenLiquid> zksIsTokenLiquid(String tokenAddress) {
+        return new Request<>(
+                "zks_isTokenLiquid", Arrays.asList(tokenAddress), web3jService, ZksIsTokenLiquid.class);
+    }
+
+    @Override
+    public Request<?, ZksTokenPrice> zksGetTokenPrice(String tokenAddress) {
+        return new Request<>(
+                "zks_getTokenPrice", Arrays.asList(tokenAddress), web3jService, ZksTokenPrice.class);
     }
     
 }
--- src/main/java/io/zksync/protocol/ZkSync.java
+++ src/main/java/io/zksync/protocol/ZkSync.java
@@ -5,10 +5,16 @@ import org.web3j.protocol.Web3jService;
 import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.Request;
 import org.web3j.protocol.core.methods.response.EthGetBalance;
+import org.web3j.protocol.core.methods.response.EthSendRawTransaction;
 
 import io.zksync.methods.request.ZksEstimateFeeRequest;
+import io.zksync.methods.response.ZksAccountType;
 import io.zksync.methods.response.ZksEstimateFee;
+import io.zksync.methods.response.ZksIsTokenLiquid;
 import io.zksync.methods.response.ZksMainContract;
+import io.zksync.methods.response.ZksTokenPrice;
+import io.zksync.methods.response.ZksTokens;
+import io.zksync.methods.response.ZksTransactions;
 
 public interface ZkSync extends Web3j {
     static ZkSync build(Web3jService web3jService) {
@@ -18,6 +24,22 @@ public interface ZkSync extends Web3j {
     Request<?, ZksEstimateFee> zksEstimateFee(ZksEstimateFeeRequest transaction);
 
     Request<?, ZksMainContract> zksMainContract();
+
+    Request<?, EthSendRawTransaction> zksGetL1WithdrawalTx(String transactionHash);
+
+    Request<?, ZksAccountType> zksGetAccountType(String address);
+
+    Request<?, ZksTransactions> zksGetAccountTransactions(String address, Integer before, Short limit);
+
+    Request<?, ZksTokens> zksGetConfirmedTokens(Integer from, Short limit);
+
+    Request<?, ZksIsTokenLiquid> zksIsTokenLiquid(String tokenAddress);
+
+    Request<?, ZksTokenPrice> zksGetTokenPrice(String tokenAddress);
+    
+    // Request<?, > zksSetContractDebugInfo();
+    // Request<?, > zksGetContractDebugInfo();
+    // Request<?, > zksGetTransactionTrace();
     
     Request<?, EthGetBalance> ethGetBalance(
         String address, DefaultBlockParameter defaultBlockParameter, String tokenAddress);
