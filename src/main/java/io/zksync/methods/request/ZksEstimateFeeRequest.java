667e57f471d5720399dc6739bc0a29a84c0aac64
MaximFischuk <mfischuk@vareger.com>
2021-12-31 13:35:52 +0200

    Add build method for create ZksEstimateFeeRequest from ZkSync transaction

--- src/main/java/io/zksync/methods/request/ZksEstimateFeeRequest.java
+++ src/main/java/io/zksync/methods/request/ZksEstimateFeeRequest.java
@@ -2,6 +2,8 @@ package io.zksync.methods.request;
 
 import org.web3j.crypto.RawTransaction;
 
+import io.zksync.abi.TransactionEncoder;
+import io.zksync.transaction.Transaction;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,11 +11,16 @@ import lombok.Data;
 @Data
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ZksEstimateFeeRequest {
-    
+
     private String data;
 
     public static ZksEstimateFeeRequest fromRawTransaction(RawTransaction transaction) {
         return new ZksEstimateFeeRequest("0x" + transaction.getData());
+    }
+
+    public static ZksEstimateFeeRequest fromZkTransaction(Transaction transaction) {
+        RawTransaction transactionForEstimate = TransactionEncoder.encodeToRawTransaction(transaction);
+        return new ZksEstimateFeeRequest("0x" + transactionForEstimate.getData());
     }
 
 }
