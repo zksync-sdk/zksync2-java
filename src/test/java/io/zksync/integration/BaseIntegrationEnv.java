0dcb1115f47fcf06834dcfe476c7f116a1b47216
petarTxFusion <petar@txfusion.io>
2024-11-17 18:49:19 +0100

    test: update test workflow

--- src/test/java/io/zksync/integration/BaseIntegrationEnv.java
+++ src/test/java/io/zksync/integration/BaseIntegrationEnv.java
@@ -152,8 +152,11 @@ public class BaseIntegrationEnv {
         System.out.println("L1 DAI balance after: " + testWallet.getBalanceL1().send());
         System.out.println("L2 DAI balance after: " + testWallet.getBalance(l2Dai).send());
 
-        String tokenAddress = deployToken();
-        deployPaymaster(tokenAddress);
+        String code = zksync.ethGetCode("0x841c43fa5d8fffdb9efe3358906f7578d8700dd4", DefaultBlockParameterName.LATEST).send().getCode();
+        if (code.isEmpty()) {
+            String tokenAddress = deployToken();
+            deployPaymaster(tokenAddress);
+        }
     }
 
     public void depositToken(String tokenAddress) throws Exception {
